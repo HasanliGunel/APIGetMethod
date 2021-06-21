c9142e9230534277fa6aa5e09fb738b59e922317
HasanliGunel <hasanligunel@hotmail.com>
2021-06-21 21:13:40 +0400

    first commit

---
+++ Controllers/Hello.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hello : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!!!");
+        }
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+
+            return Ok(payload);
+        }
+
+    }
+}
